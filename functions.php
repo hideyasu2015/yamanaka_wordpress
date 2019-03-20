64bc9c87945fee53ad4e5b090f1527c994685835
chihiro <sense.and.sensibility0112@gmail.com>
2019-03-21 00:06:51 +0900

    img change fax tel remove

--- functions.php
+++ functions.php
@@ -1,29 +1,6 @@
 <?php
   add_theme_support('menus');
-//
-//   add_filter('nav_menu_css_class', 'my_nav_menu_li', 10, 2);
-//   function my_nav_menu_li( $classes, $item ){
-//      $classes = array();
-//      if( $item -> current == true ) {
-//        $classes[] = 'nav-item active';
-//      }else{
-//        $classes[] = 'nav-item';
-//      }
-//      return $classes;
-//   }
-//
-//   add_filter( 'nav_menu_link_attributes', 'filter_function_name', 10, 3 );
-//   function filter_function_name( $atts, $item, $args ) {
-//     $atts['class'] = 'nav-link pl-1';
-//     return $atts;
-// }
-  ?>
-  <?php
-  /*
-  Bootstrap 4.0.0-alpha2 nav walker extension class
-  =================================================
-  Add this to your `functions.php`
-  */
+
   class bootstrap_4_walker_nav_menu extends Walker_Nav_menu {
 
       function start_lvl( &$output, $depth ){ // ul
--- single-job_content.php
+++ single-job_content.php
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row py-2 mx-xs-5 m-2" data-aos-offset="300" data-aos="fade-in" data-aos-duration="1500">
         <div class="card bg-dark text-black border-light">
-          <img class="card-img" src="<?php echo get_template_directory_uri(); ?>/images/bg_img_law01.png" alt="">
+          <img class="card-img" src="<?php echo get_template_directory_uri(); ?>/images/various_images.png" alt="">
           <div class="card-img-overlay">
             <h3 class="card-title" data-aos="fade-in" data-aos-delay="400" data-aos-easing="linear" data-aos-duration="800">公図・地積測量図・登記の経緯等の登記情報調査</h3>
           </div>
